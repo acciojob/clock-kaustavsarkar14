b9ac05b843292c1197964bfb2259e102c89ee934
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-19 11:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const timer = document.getElementById('timer')
+
+function setTime(){
+	const date = new Date()
+	timer.innerText = date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+", "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+" "+(date.getHours()>12?"PM":"AM")
+}
+setInterval(()=>{
+setTime()
+},1000)
